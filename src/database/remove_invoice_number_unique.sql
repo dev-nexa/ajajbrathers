9a89e195288ad6915840d48304b6bbadd1828f31
Anas Kilani <149209587+anas-8888@users.noreply.github.com>
2025-06-24 20:23:06 +0300

    Delete src directory

--- src/database/remove_invoice_number_unique.sql
+++
@@ -1,2 +0,0 @@
--- Remove UNIQUE constraint from invoice_number column
-ALTER TABLE invoices DROP INDEX invoice_number; 
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { isAuthenticated } = require('../middleware/auth');
-const { getHome } = require('../controllers/homeController');
-
-// الصفحة الرئيسية - يجب تسجيل الدخول للوصول إليها
-router.get('/', isAuthenticated, getHome);
-
-module.exports = router; 
